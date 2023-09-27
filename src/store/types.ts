b34d2dcc3acff2833a8bd123a99bf4a7fd6c11bc
Silverium <soldeplatadeveloper@gmail.com>
2023-09-27 10:44:46 +0200

    fix build

--- src/store/types.ts
+++ src/store/types.ts
@@ -1,3 +1,25 @@
+export interface Attributes {
+  attributes?: Attributes;
+  height?: string;
+    rel?: string;
+  type?: string;
+  href?: string;
+  'im:id'?: string;
+  term?: string;
+  scheme?: string;
+  label?: string;
+  amount?: string;
+  currency?: string;
+}
+export interface ImContentType {
+  'im:contentType': Attributes;
+  attributes: Attributes;
+}
+
+export interface Id {
+  attributes: Attributes;
+}
+
 export interface Album {
   'im:name': Attributes;
   'im:image'?: (Attributes)[] | null;
@@ -12,29 +34,6 @@ export interface Album {
   category: Attributes;
   'im:releaseDate': Attributes;
 }
-
-export interface Attributes {
-  attributes?: Attributes;
-  height?: string;
-    rel?: string;
-  type?: string;
-  href?: string;
-  'im:id'?: string;
-  term?: string;
-  scheme?: string;
-  label?: string;
-  amount?: string;
-  currency?: string;
-}
-export interface Id {
-  attributes: Attributes;
-}
-
-export interface ImContentType {
-  'im:contentType': Attributes;
-  attributes: Attributes;
-}
-
 
 export interface RootState {
     albums: Album[];
